f1c86d0ab57892a272702861a967d98aad3480ae
Troy Appleby <Troy.appleby@mths.ca>
2023-01-18 15:55:43 +0000

    all new files

---
+++ lesson-01/main.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Troy Appleby
+//
+// Created by: Troy Appleby
+// Created on: Jan 2023
+// This program is the Space Aliens game for the Gameboy
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main(){
+   printf("Hello, World!");
+}
